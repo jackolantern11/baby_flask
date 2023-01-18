63082b0bd1e559f11c19b8149faa68d3aec73bc9
z0f010w <zachary.freeze0@walmart.com>
2023-01-18 11:26:47 -0600

    updated to itertuples for faster loading, added simple dummy matplotlib graph using a png buffer file

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 from application import app
-from flask import render_template
+from flask import render_template, send_file
 import pandas as pd
+import matplotlib.pyplot as plt
+import io
 
 @app.route('/')
 @app.route('/index')
@@ -8,9 +10,27 @@ import pandas as pd
 def index():
     return render_template("index.html")
 
+
+@app.route('/generate_png')
+def generate_png():
+
+    df = pd.read_csv("data.csv").head(25)
+    plt.plot(df['year'], df['name'])
+    plt.xlabel('Year')
+    plt.ylabel('Name')
+    plt.title('My First Flask Graph')
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    return send_file(buf, mimetype='image/png')
+
+
 @app.route('/data_summary')
 def data_summary():
     # page to display a dataframe as a table
+    # @ToDo - How to update this yearly or manually?
     df = pd.read_csv("data.csv")
     return render_template("data_summary.html", data={'year_min': df['year'].min(),
                                                       'year_max': df['year'].max()}, df=df)
