c8d0bcc66af102486defc94f3f7227a287313260
zfreeze <zdfreeze11@gmail.com>
2023-01-20 13:35:19 -0600

    added simple plotly plot with JS to create a graph

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,8 @@
 from application import app
-from flask import render_template, send_file
+from flask import render_template
 import pandas as pd
-import matplotlib.pyplot as plt
-import io
+import plotly.graph_objs as go
+import plotly.io as pio
 
 @app.route('/')
 @app.route('/index')
@@ -10,27 +10,21 @@ import io
 def index():
     return render_template("index.html")
 
+@app.route('/graph')
+def graph():
 
-@app.route('/generate_png')
-def generate_png():
+    df = pd.read_csv("data.csv").head(100)
+    data = [go.Scatter(x=df['year'], y=df['name'])]
+    layout = go.Layout(title='My Graph')
+    fig = go.Figure(data=data, layout=layout)
+    graphJSON = pio.to_json(fig)
 
-    df = pd.read_csv("data.csv").head(25)
-    plt.plot(df['year'], df['name'])
-    plt.xlabel('Year')
-    plt.ylabel('Name')
-    plt.title('My First Flask Graph')
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    return send_file(buf, mimetype='image/png')
-
+    return render_template('graph.html', graphJSON=graphJSON)
 
 @app.route('/data_summary')
 def data_summary():
     # page to display a dataframe as a table
     # @ToDo - How to update this yearly or manually?
-    df = pd.read_csv("data.csv")
+    df = pd.read_csv("data.csv").head(25)
     return render_template("data_summary.html", data={'year_min': df['year'].min(),
                                                       'year_max': df['year'].max()}, df=df)
