e402ae8d298b464e0f574ed7da34140ffbec27d7
zfreeze <zdfreeze11@gmail.com>
2025-02-17 20:05:54 -0600

    add index changes to models.py

--- app/models.py
+++ app/models.py
@@ -7,12 +7,12 @@ import pandas as pd
 class Federal_Data(db.Model):
 
     __tablename__ = 'federal_data'
-    _id = db.Column(db.Integer, primary_key=True, unique=True)
+    index = db.Column(db.Integer, primary_key=True, unique=True)
     year = db.Column(db.Integer)
     name = db.Column(db.Integer)
     gender = db.Column(db.String(1))
     count = db.Column(db.Integer)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    # timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     @staticmethod
     def transform_objects_to_df(object_list: list) -> pd.DataFrame:
